cce80987e5884abd1c01b8b3128f2f88b341b635
Horoshiy <admin@2liga.ru>
2020-11-23 19:31:14 +0300

    fix errors and optimize methods

--- src/test/java/AlfaBankTests.java
+++ src/test/java/AlfaBankTests.java
@@ -1,32 +1,26 @@
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byAttribute;
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AlfaBankTests {
-
-    @BeforeEach
-    void prepare() {
-        open("https://alfabank.ru/make-money/");
-    }
-
     @Test
     void archiveDepositsCountTest() {
-        $(byAttribute("title", "Вклады")).hover();
+        open("https://alfabank.ru/make-money/");
+        $(by("title", "Вклады")).hover();
         $(byText("Депозиты")).closest("a").click();
         $(byText("Архивные счета и депозиты")).scrollTo().click();
-        $(byAttribute("data-test-id", "tabs-list")).$(byText("Депозиты")).closest("button").click();
-        $$(byAttribute("data-widget-name", "CatalogCard")).shouldHave(CollectionCondition.size(5));
+        $(by("data-test-id", "tabs-list")).$(byText("Депозиты")).closest("button").click();
+        $$(by("data-widget-name", "CatalogCard")).shouldHaveSize(5);
     }
 
     @Test
-    void insuranceWithSiblingTest() {
+    void insuranceWithSiblingAndPreceedingAndClosestTest() {
+        open("https://alfabank.ru/make-money/");
         SelenideElement element = $(byText("Страхование вкладов")).parent();
         element.sibling(0).shouldHave(text("Что такое вклад"));
         element.preceding(0).shouldHave(text("Описание"));
